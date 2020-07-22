33c614625a4a386f2ada11d94b055917a1a82b17
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-07-22 11:56:56 +0200

    Update 'works' test for the new API

--- tests/works.rs
+++ tests/works.rs
@@ -1,38 +1,25 @@
-// use futures::channel::oneshot::{self, Canceled};
-// use futures::prelude::*;
-// use std::io;
-// use tingle::*;
+use async_backplane::*;
+use futures::prelude::*;
+use std::thread;
 
-// #[test]
-// fn works() {
-//     let (sender, recver) = oneshot::channel();
-//     drop(sender);
+#[test]
+fn works() {
+    let mut d1 = Device::new();
+    let d2 = Device::new();
 
-//     let mut p1 = Particle::new::<(), io::Error>(future::pending());
-//     let p2 = Particle::new::<(), io::Error>(future::pending());
-//     let mut p3 = Particle::new::<(), Canceled>(recver);
-//     let p4 = Particle::new::<(), io::Error>(async {
-//         panic!("catch that");
-//     });
-//     let p5 = Particle::new::<(), io::Error>(future::pending());
-//     let p6 = Particle::new::<(), io::Error>(future::ready(Ok(())));
+    d1.link(d2.open_line()).expect("failed linking");
 
-//     p1.entangle(p2.as_wave());
-//     p3.entangle(p2.as_wave());
-//     p5.as_wave().cancel();
+    let i1 = d1.device_id();
+    let m1 = d1.monitoring(future::pending());
 
-//     smol::run(async {
-//         // > Error::Particle(oneshot canceled)
-//         assert!(p3.await.is_err());
-//         // > Error::Entangled (p3 -> w2 && p1 <- w2)
-//         assert!(p1.await.is_err());
-//         // > Error::Entangled (p3 -> w2)
-//         assert!(p2.await.is_err());
-//         // > Error::Panic(Any)
-//         assert!(p4.await.is_err());
-//         // > Ok(None)
-//         assert_eq!(p5.await.unwrap(), None);
-//         // > Ok(Some(()))
-//         assert_eq!(p6.await.unwrap(), Some(()));
-//     });
-// }
+    let t1 = thread::spawn(move || smol::block_on(d2.disconnect(Disconnect::Complete)));
+    let t2 = thread::spawn(|| smol::block_on(m1));
+
+    let _ = t1.join().unwrap();
+    let r2 = t2.join().unwrap();
+
+    match r2.unwrap_err() {
+        Crash::Cascade(id, Disconnect::Complete) => assert_eq!(id, i1),
+        _ => panic!("unexpected"),
+    }
+}
